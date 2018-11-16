136d272540db1c29a077aa3fd76663f8cfc20be1
akshata.akkanna@gmail.com <akshata.akkanna@gmail.com>
2018-11-16 17:13:56 +0530

    Solidity Basic examples

---
+++ Basic_token.sol
@@ -0,0 +1,16 @@
+pragm solidity ^0.4.0;
+
+contract BasicToken{
+	
+	mapping(address => uint256) balances;
+
+	function trasfer(address recepient, uint256 value){
+ 			
+ 			balances[msg.sender] - = value;
+ 			balances[msg.recepient] + = value;
+	}
+	function balanceof(address account) public constant returns(unint256){
+
+		return balance[account];
+	}
+}
